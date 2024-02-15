ca4eb8ca329b79ef10c4617526f973107c8d27df
Rawk Akani <lionelmbankeu@gmail.com>
2024-02-15 13:51:12 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: [
+    "./src/_app/**/*.{html,js,svg,css,jsx}",
+    "./src/_app/*.{html,js,svg,css,jsx}",
+    "./src/components/**/*.{html,js,svg,css,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
